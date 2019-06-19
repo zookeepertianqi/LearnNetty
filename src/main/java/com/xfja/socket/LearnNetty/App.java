608287a022f6909083b9c2188612479a81ef5fa3
lenovo <lenovo@lenovo-PC>
2019-06-19 15:35:00 +0800

    首次提交netty编程代码

---
+++ src/main/java/com/xfja/socket/LearnNetty/App.java
@@ -0,0 +1,13 @@
+package com.xfja.socket.LearnNetty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
